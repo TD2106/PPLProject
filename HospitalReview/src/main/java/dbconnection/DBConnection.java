37b17394ae7fbf23814949b3198c461904ce62c9
TD2106 <thanhduy2106@gmail.com>
2018-04-23 01:01:48 +0700

    Added DBConnection and AES encryption, decryption

---
+++ HospitalReview/src/main/java/dbconnection/DBConnection.java
@@ -0,0 +1,22 @@
+package dbconnection;
+
+import java.sql.*;
+
+/**
+ * @author Duy Le
+ */
+public class DBConnection {
+    private static Connection dbConnection;
+    private static String connectionURL = "jdbc:mysql://localhost:3306/hospitalreview";
+    private static String user = "root";
+    private static String password = "lutden";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        if (dbConnection == null) {
+            dbConnection = DriverManager.getConnection(connectionURL, user, password);
+        }
+        return dbConnection;
+    }
+
+}
